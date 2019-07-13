dfe776d741d49d06a5dc452a49ae449bc435c1aa
Gabriel Vieira <viera_frifri@hotmail.com>
2019-07-13 18:42:15 -0300

    added rgb function

--- yeelight/Scenes/LightController/LightControllerViewController.swift
+++ yeelight/Scenes/LightController/LightControllerViewController.swift
@@ -74,30 +74,27 @@ extension LightControllerViewController: ChromaColorPickerDelegate {
             self.device.changeColor(Int(result))
         }
     }
+}
+
+extension UIColor {
     
-    func rgbToHue(r:CGFloat,g:CGFloat,b:CGFloat) -> (h:CGFloat, s:CGFloat, b:CGFloat) {
-        
-        let minV:CGFloat = CGFloat(min(r, g, b))
-        let maxV:CGFloat = CGFloat(max(r, g, b))
-        let delta:CGFloat = maxV - minV
-        var hue:CGFloat = 0
-        if delta != 0 {
-            if r == maxV {
-                hue = (g - b) / delta
-            }
-            else if g == maxV {
-                hue = 2 + (b - r) / delta
-            }
-            else {
-                hue = 4 + (r - g) / delta
-            }
-            hue *= 60
-            if hue < 0 {
-                hue += 360
-            }
+    func rgb() -> Int? {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
+            let iRed = Int(fRed * 255.0)
+            let iGreen = Int(fGreen * 255.0)
+            let iBlue = Int(fBlue * 255.0)
+            let iAlpha = Int(fAlpha * 255.0)
+            
+            //  (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are blue).
+            let rgb = (iAlpha << 24) + (iRed << 16) + (iGreen << 8) + iBlue
+            return rgb
+        } else {
+            // Could not extract RGBA components:
+            return nil
         }
-        let saturation = maxV == 0 ? 0 : (delta / maxV)
-        let brightness = maxV
-        return (h:hue/360, s:saturation, b:brightness)
     }
 }
